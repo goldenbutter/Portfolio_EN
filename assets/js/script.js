0a36299097a646ab634cc1d29cd05405cb5c2f81
Bithun Chatterjee <bithun.chatterjee@gmail.com>
2026-02-07 12:22:09 +0100

    Navigation bar added

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,19 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 });
+
+
+// Scroll to top button
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
